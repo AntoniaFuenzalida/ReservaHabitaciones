d79c99eaa0a33fa18a0c78031ae28d97b6fdbba0
Roberto Cañete <robertoandrecane@hotmail.com>
2023-04-20 02:30:14 -0400

    Vista Menu Home
    Es la vista home de la pagina del hotel, falta arreglar detalles

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-main>
+      <VistaHome/>
+    </v-main>
+</template>
+
+<script>
+import VistaHome from './components/VistaHome';
+
+export default {
+  name: 'App',
+
+  components: {
+    VistaHome,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
