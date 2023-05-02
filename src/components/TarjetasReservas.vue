621a02f4cb6e8bbf575dbdfe9546f7210e540041
Jorge Migueles <jmigueles@alumnos.utalca.cl>
2023-05-02 16:31:25 -0400

    Tarjetas que se crean mediante una lista

---
+++ src/components/TarjetasReservas.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    props: {
+        habitacion: String,
+        precio: String,
+        estado: Boolean,
+    },
+    data: () => ({
+        Mostrar: false
+    })
+}
+</script>
+<template>
+    <div class="card h-60" >
+        <!-- Product image-->
+        <img class="card-img-top" src="https://i.imgur.com/lg0Vhr6.jpg" alt="..." />
+        <!-- Product details-->
+        <div class="card-body p-4">
+            <div class="text-center">
+                <!-- Product name-->
+                <h5 class="fw-bolder">{{ habitacion }} </h5>
+                <!-- Product reviews-->
+                <div class="d-flex justify-content-center small text-warning mb-2">
+
+                </div>
+                <!-- Product price-->
+                {{ precio }} p/noche
+            </div>
+            <div class="text-center">
+                <!-- Product name-->
+                <button type="button" class="btn btn-dark" > Haz tu Reserva</button>
+            </div>
+        </div>
+    </div>
+
+</template>
