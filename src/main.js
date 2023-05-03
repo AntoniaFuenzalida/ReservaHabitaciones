0744c4b988a62f97c7d7c62cba37b405717efbe5
Jorge Migueles <jmigueles@alumnos.utalca.cl>
2023-05-03 17:11:59 -0400

    coneccion
    base de datos

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+  import { initializeApp } from "firebase/app";
+  import { getFirestore } from 'firebase/firestore';
 
-createApp(App).use(router).mount('#app')
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyANUceU5lF2h99d4UnabyJ1x8NcT-eKFMg",
+    authDomain: "proyectoconstru-c7c2c.firebaseapp.com",
+    databaseURL: "https://proyectoconstru-c7c2c-default-rtdb.firebaseio.com",
+    projectId: "proyectoconstru-c7c2c",
+    storageBucket: "proyectoconstru-c7c2c.appspot.com",
+    messagingSenderId: "316944078663",
+    appId: "1:316944078663:web:137ef9de03047f6abf5e62"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Get a Firestore instance using the full version of Firestore
+  const db = getFirestore(app);
+  export { db };
+
+createApp(App).use(router).mount('#app');
