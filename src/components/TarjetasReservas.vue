970af3e898fb15d6401df0623e791656184aac53
Jorge Migueles <jmigueles@alumnos.utalca.cl>
2023-05-16 22:04:32 -0400

    funcionamiento reserva y servicio adicional
    funcionamiento base aprox del 80% completado solo faltan comprobaciones minimas

--- src/components/TarjetasReservas.vue
+++ src/components/TarjetasReservas.vue
@@ -20,10 +20,6 @@
             :data-bs-target="'#MenuComida-' + reserva.numero" style="margin: 5px;" @click="selectedRoom = reserva.numero">
             Reservar
           </button>
-          <button type="button" class="btn btn-primary" style="margin: 5px;"
-            @click="$router.push('/servicios-adicionales')">
-            Ir a Servicios Adicionales
-          </button>
           <div class="modal fade" :id="'MenuComida-' + reserva.numero" tabindex="-1"
             :aria-labelledby="'exampleModalLabel-' + reserva.numero" aria-hidden="true">
             <div class="modal-dialog">
@@ -31,13 +27,16 @@
                 <div class="modal-header">
                   <h1 class="modal-title fs-4" :id="'exampleModalLabel-' + reserva.numero">
                     Habitación N° {{ selectedRoom }}</h1>
+                    fecha: {{ this.fechaFin  }}
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
+                  <h3>se realizo la reserva para las fechas</h3>
+                  <h5>fecha inicio: {{ this.fechaInicio  }} y fecha Fin: {{ this.fechaFin }}</h5>
                 </div>
                 <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                  <button type="button" class="btn btn-primary">Ir a Servicios
+                  <button  type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                  <button @click="guardarReserva(this.selectedRoom )" type="button" class="btn btn-primary">Ir a Servicios
                     Adicionales</button>
 
                 </div>
@@ -51,14 +50,61 @@
 </template>
 
 <script>
+    import router from '@/router';
+import app from '../main'
+    import { doc, getFirestore, setDoc,getDocs,collection} from "firebase/firestore";
+
+
 export default {
   props: {
-    reserva: Object
+    reserva: Object,
+    fechaFin: {
+      type: Date,
+      default: null
+    },
+    fechaInicio: {
+      type: Date,
+      default: null
+    }
   },
   data() {
     return {
       selectedRoom: ''
     }
+  },
+  methods: {  
+  async guardarReserva(numeroHabitacion) {
+    const db = getFirestore(app);
+    const querySnapshot = await getDocs(collection(db, "Reservas"));  
+
+
+
+
+
+
+
+    const reser = {
+      cantidadCamas: "test",
+      cantidadPersonas: "test",
+      estadoReserva: "pendiente",
+      fechaIngreso: String(this.fechaInicio),
+      fechaSalida: String(this.fechaFin),
+      idReserva: "n"+querySnapshot.size,
+      nombreCliente: "usuario prueba",
+      numeroHabitacion: String(numeroHabitacion),
+      rut: "test rut"
+    };
+
+    const variable="n"+String(querySnapshot.size);
+    // Guardar reserva en Firebase
+    await setDoc(doc(db, "Reservas",variable), reser);
+   // console.log("se registro:" + reser)
+    router.push({ name: 'Servicios_Adicionales', query: { variable } })
+
   }
 }
+
+}
+
+
 </script>
