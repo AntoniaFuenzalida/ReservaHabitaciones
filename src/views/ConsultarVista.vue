8cbedd5d21fb57f467e50997c890c5dd725c8055
Jorge Migueles <jmigueles@alumnos.utalca.cl>
2023-05-20 15:04:03 -0400

    agregar limites a la busqueda y ver si la fecha no es una fecha

--- src/views/ConsultarVista.vue
+++ src/views/ConsultarVista.vue
@@ -4,72 +4,36 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container px-4 px-lg-5">
             <a class="navbar-brand" href="/">
-                <img
-                    src="https://hotelcordillera.cl/wp-content/uploads/2021/11/logo.jpg"
-                    height="50"
-                    alt="hotel logo"
-                    loading="lazy"
-                    style="margin-top: -1px"
-            /></a>
+                <img src="https://hotelcordillera.cl/wp-content/uploads/2021/11/logo.jpg" height="50" alt="hotel logo"
+                    loading="lazy" style="margin-top: -1px" /></a>
 
-            <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="offcanvas"
-                data-bs-target="#offcanvasNavbar"
-                aria-controls="offcanvasNavbar"
-                aria-label="Toggle navigation"
-            >
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+                aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div
-                class="offcanvas offcanvas-end"
-                tabindex="-1"
-                id="offcanvasNavbar"
-                aria-labelledby="offcanvasNavbarLabel"
-            >
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                 <div class="offcanvas-header">
-                    <button
-                        type="button"
-                        class="btn-close"
-                        data-bs-dismiss="offcanvas"
-                        aria-label="Close"
-                    ></button>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="offcanvas-body">
                     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                         <li class="nav-item dropdown">
-                            <a
-                                class="nav-link dropdown-toggle"
-                                href="#"
-                                role="button"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                            >
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
                                 Menú
                             </a>
                             <ul class="dropdown-menu">
                                 <li>
-                                    <a
-                                        class="dropdown-item"
-                                        href="./menu_Usuario"
-                                        >Perfil</a
-                                    >
+                                    <a class="dropdown-item" href="./menu_Usuario">Perfil</a>
                                 </li>
                                 <li>
-                                    <a class="dropdown-item" href="./"
-                                        >Cerrar sesión</a
-                                    >
+                                    <a class="dropdown-item" href="./">Cerrar sesión</a>
                                 </li>
                                 <li>
                                     <hr class="dropdown-divider" />
                                 </li>
                                 <li>
-                                    <a
-                                        class="dropdown-item"
-                                        href="Servicios_Adicionales"
-                                        >Servicios adicionales</a
-                                    >
+                                    <a class="dropdown-item" href="Servicios_Adicionales">Servicios adicionales</a>
                                 </li>
                             </ul>
                         </li>
@@ -93,37 +57,17 @@
                 <br />
 
                 <form>
-                    <input
-                        type="date"
-                        id="start"
-                        min="2018-01-01"
-                        v-model="fecha_inicio"
-                    />
-                    <input
-                        type="date"
-                        id="fin"
-                        min="2018-01-01"
-                        v-model="fecha_fin"
-                    />
+                    <input type="date" id="start" min="2018-01-01" v-model="fecha_inicio" />
+                    <input type="date" id="fin" min="2018-01-01" v-model="fecha_fin" />
                     <div class="form-group w-25">
                         <br />
                         <label for="inputsm"> Cantidad Adultos</label>
-                        <input
-                            class="form-control input-sm"
-                            id="inputsm"
-                            type="text"
-                            v-model="adultos"
-                        />
+                        <input class="form-control input-sm" id="inputsm" type="text" v-model="adultos" />
                     </div>
                     <br />
                     <div class="form-group w-25">
                         <label for="inputdefault">Cantidad Niños</label>
-                        <input
-                            class="form-control"
-                            id="inputdefault"
-                            type="text"
-                            v-model="kid"
-                        />
+                        <input class="form-control" id="inputdefault" type="text" v-model="kid" />
                     </div>
                 </form>
             </div>
@@ -133,15 +77,9 @@
 
     <section class="py-5">
         <div class="container px-4 px-lg-5 mt-5">
-            <div
-                class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center"
-            >
+            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                 <div class="col mb-3" v-for="reserva in final" :key="reserva">
-                    <Tarjetas
-                        :reserva="reserva"
-                        :fecha-fin="fecha_fin"
-                        :fecha-inicio="fecha_inicio"
-                    />
+                    <Tarjetas :reserva="reserva" :fecha-fin="fecha_fin" :fecha-inicio="fecha_inicio" />
                 </div>
             </div>
         </div>
@@ -155,6 +93,8 @@
             </p>
         </div>
     </footer>
+
+    <!--pop up error-->
 </template>
 
 <script>
@@ -188,10 +128,59 @@ export default {
 
     methods: {
         async reservaFiltradas() {
-            if (this.fecha_inicio >= this.fecha_fin) {
+
+            //creo variables auxiliares para hacer la verificacion si las fechas son validas(que no sean menor a hoy )
+            let auxFechainicio = new Date(this.fecha_inicio);
+            let auxFechaFin = new Date(this.fecha_fin);
+            let fechaHoy = new Date();
+            auxFechaFin.setDate(auxFechaFin.getDate() + 1);
+            auxFechainicio.setDate(auxFechainicio.getDate() + 1);
+            fechaHoy.setHours(0, 0, 0, 0);
+            auxFechainicio.setHours(0, 0, 0, 0)
+            auxFechaFin.setHours(0, 0 ,0 ,0)
+            console.log(fechaHoy + "llegada:" + auxFechainicio + "salida:" + auxFechaFin)
+
+            //diferencia entre hoy y la de llegada
+
+            var diferenciaMs = Math.abs(auxFechainicio - fechaHoy);
+
+            // Convertir la diferencia en días
+            let dif_ini_hoy = Math.ceil(diferenciaMs / (1000 * 60 * 60 * 24));
+
+
+
+            // diferencia entre inicio y salida
+             diferenciaMs = Math.abs(auxFechaFin - auxFechainicio);
+            let dif_ini_fin = Math.ceil(diferenciaMs / (1000 * 60 * 60 * 24));
+
+
+            //diferencia entre la salida y hoy
+             diferenciaMs = Math.abs(auxFechaFin - fechaHoy);
+            let dif_hoy_fin = Math.ceil(diferenciaMs / (1000 * 60 * 60 * 24));
+
+
+
+                //verifica que la fecha no se salga de los limites de un año
+                if(dif_ini_hoy>=365||dif_ini_fin>=365||dif_hoy_fin>=365)
+                {
                 this.fecha_fin = NaN;
                 this.fecha_inicio = NaN;
+                this.final=[]
+                console.log("la fecha no esta fuera de rango");
+
+                }
+
+            //verifica que la fecha sea valida
+            if (auxFechainicio >= auxFechaFin || auxFechainicio < fechaHoy || auxFechaFin <= fechaHoy) {
+                this.fecha_fin = NaN;
+                this.fecha_inicio = NaN;
+                this.final=[]
+                console.log("la fecha no es valida");
             } else {
+
+
+
+
                 let fechaInicio = new Date(this.fecha_inicio);
                 let fechaFin = new Date(this.fecha_fin);
 
@@ -253,10 +242,10 @@ export default {
 
                 ///console.log(result)
                 this.final = [];
-
                 if (!Number(this.adultos) && this.adultos != "0") {
                     this.fecha_fin = NaN;
                     this.fecha_inicio = NaN;
+                    this.final=[]
                     console.log(
                         "El numero de adultos debe ser un numero positivo"
                     );
@@ -264,12 +253,14 @@ export default {
                 if (!Number(this.kid) && this.kid != "0") {
                     this.fecha_fin = NaN;
                     this.fecha_inicio = NaN;
+                    this.final=[]
                     console.log(
                         "El numero de niños debe ser un numero positivo"
                     );
                 }
-                setCookie('ninos',this.kid,1)
-                setCookie('adultos',this.adultos,1)
+
+                setCookie('ninos', this.kid, 1)
+                setCookie('adultos', this.adultos, 1)
 
                 let tamanno = Number(this.adultos) + Number(this.kid) / 2;
                 result.forEach((doc) => {
@@ -291,11 +282,11 @@ export default {
 };
 
 function setCookie(nombre, valor, expiracion) {
-      var fechaExpiracion = new Date();
-      fechaExpiracion.setTime(fechaExpiracion.getTime() + expiracion * 24 * 60 * 60 * 1000);
-      var cookie = nombre + '=' + encodeURIComponent(valor) + '; expires=' + fechaExpiracion.toUTCString() + '; path=/';
-     document.cookie = cookie;
-    }
+    var fechaExpiracion = new Date();
+    fechaExpiracion.setTime(fechaExpiracion.getTime() + expiracion * 24 * 60 * 60 * 1000);
+    var cookie = nombre + '=' + encodeURIComponent(valor) + '; expires=' + fechaExpiracion.toUTCString() + '; path=/';
+    document.cookie = cookie;
+}
 
 
 </script>
