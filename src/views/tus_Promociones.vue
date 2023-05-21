10e9e8c91044310c357c3c8360d1bd4b3c39cda6
alfajor <mcamilla20@alumnos.utalca.cl>
2023-05-21 19:37:16 -0400

    Arreglado la estructura para Trabajar Promociones
    Además hay un pequeño avance para las funcionalidades

--- src/views/tus_Promociones.vue
+++ src/views/tus_Promociones.vue
@@ -1,4 +1,179 @@
 
+
+
+
+<script>
+import app from '../main'
+import { doc, getFirestore, setDoc, getDocs, collection } from "firebase/firestore";
+import { db } from "../main.js";
+export default {
+
+
+  data() {
+    return {
+      nombre: "uwu",
+      reservas: [],
+    }
+  },
+
+  created() {
+    this.nombre = this.buscarUSuario(this.getCookie('usuarioRegistrado')).Nombre_Apellido
+    this.reservas = this.buscarReservas()
+
+  },
+
+  methods: {
+    async guardarDatos() {
+      console.log("Guardando")
+      const db = getFirestore(app);
+      if (await this.datosCorrectos(true) === 1) {
+        await setDoc(doc(db, "Habitaciones", this.numero), {
+          cantidadCamas: this.cantidadCamas,
+          descripcion: this.descripcion,
+          numero: this.numero,
+          precio: this.precio,
+        })
+        console.log("termine de guardar")
+      } else {
+        console.log("Datos Incorrectos")
+      }
+    },
+    async modificarDatos(numero) {
+      console.log(numero)
+      const db = getFirestore(app);
+      this.numero = numero   // esto es para validar los datos
+      if (await this.datosCorrectos(false) == 1) {
+        await setDoc(doc(db, "Habitaciones", numero), {
+          cantidadCamas: this.cantidadCamas,
+          descripcion: this.descripcion,
+          numero: numero,
+          precio: this.precio,
+        })
+      } else {
+        console.log("Datos Incorrectos")
+      }
+    },
+    async datosCorrectos(rep) {
+      const db = getFirestore(app);
+      const querySnapshot = await getDocs(collection(db, "Habitaciones"));
+      var correcto = 1
+
+      // numero valido      
+      if (!Number(this.numero) && this.numero != "0") {
+        correcto = 0
+        console.log("El numero de habitacion debe ser un numero")
+      }
+      else if (this.numero < 1) {
+        correcto = 0
+        console.log("El numero de habitacion debe ser positivo")
+      } else if (rep) {
+        // numero de habitacion repetido
+        querySnapshot.forEach((doc) => {
+          if (doc.data().numero == this.numero) {
+            correcto = 0
+            console.log("Numero repetido")
+          }
+        },
+        );
+      }
+
+      // precio valido
+      if (!Number(this.precio) && this.precio != "0") {
+        correcto = 0
+        console.log("El precio de la habitacion debe ser un numero")
+      } else if (this.precio < 1) {
+        correcto = 0
+        console.log("El precio de la habitacion debe ser positivo")
+      }
+
+      // cantidadCamas valido
+      if (!Number(this.cantidadCamas) && this.cantidadCamas != "0") {
+        correcto = 0
+        console.log("La cantidad de camas de la habitacion debe ser un numero")
+      } else if (this.cantidadCamas < 1) {
+        correcto = 0
+        console.log("La cantidad de camas de la habitacion debe ser positiva")
+      }
+      return correcto;
+    },
+
+    async buscarUSuario(correo) {
+      const usuarios = await getDocs(collection(db, "Cuentas"));
+      usuarios.forEach((doc) => {
+        var accountData = doc.data();
+        if (accountData.Correo_Electronico == correo) {
+
+          console.log(accountData)
+          this.rut = accountData.Rut
+          this.nombre = accountData.Nombre_Apellido
+          this.telefono = accountData.Telefono
+
+          return (accountData)
+        }
+      });
+
+    },
+
+    async buscarReservas() {
+      var usuario_reservas = []
+      const resul = await getDocs(collection(db, "Reservas"));
+      resul.forEach((doc) => {
+        var accountData = doc.data();
+        if (accountData.Correo_Electronico == this.getCookie('usuarioRegistrado')) {
+          this.reservas.push(accountData)
+          usuario_reservas.push(accountData)
+          console.log(accountData)
+        }
+
+
+      }
+      );
+      console.log(usuario_reservas)
+      return (usuario_reservas)
+
+    },
+
+    getCookie(nombre) {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].trim();
+        if (cookie.startsWith(nombre + '=')) {
+          return decodeURIComponent(cookie.substring(nombre.length + 1));
+        }
+
+      }
+      return null;
+    },
+
+    setCookie(nombre, valor, expiracion) {
+      var fechaExpiracion = new Date();
+      fechaExpiracion.setTime(
+        fechaExpiracion.getTime() + expiracion * 24 * 60 * 60 * 1000
+      );
+      var cookie =
+        nombre +
+        "=" +
+        encodeURIComponent(valor) +
+        "; expires=" +
+        fechaExpiracion.toUTCString() +
+        "; path=/";
+      document.cookie = cookie;
+    }
+
+
+
+  }
+}
+
+</script>
+
+<script setup>
+
+</script>
+
+
+
+
 <template>
   <!-- -------------          header               ------------ -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -6,7 +181,7 @@
       <a class="navbar-brand" href="/">
         <img src="https://hotelcordillera.cl/wp-content/uploads/2021/11/logo.jpg" height="50" alt="hotel logo"
           loading="lazy" style="margin-top: -1px;" /></a>
-
+      {{ nombre }}
       <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
         aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -15,7 +190,7 @@
         <div class="offcanvas-header">
           <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
-        <a> {{ nombre }} </a>
+
         <div class="offcanvas-body">
           <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
             <li class="nav-item dropdown">
@@ -76,33 +251,11 @@
   </footer>
 </template>
 
-<script setup>
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from "../main.js";
 
-function getCookie(nombre) {
-  var cookies = document.cookie.split(';');
-  for (var i = 0; i < cookies.length; i++) {
-    var cookie = cookies[i].trim();
-    if (cookie.startsWith(nombre + '=')) {
-      return decodeURIComponent(cookie.substring(nombre.length + 1));
-    }
-  }
-  return null;
-}
-let nombre = getCookie('usuarioRegistrado')
 
-</script>
 
-<script>
-let nombreActivo
 
-export default {
-  methods: {
-    
-  },
-}
-</script>
+
 
 
 <style scoped>
